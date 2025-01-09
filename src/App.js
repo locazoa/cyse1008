f13549df7a33a32361322ace206968dc51acd289
Cliff Coulter <ccoulter@loyalistcollege.com>
2025-01-09 11:11:18 -0500

    my first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello CYSE 1008!
+          Hello CYSE 1008 2025!
         </a>
       </header>
     </div>
